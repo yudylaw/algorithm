e9ac35c2f139d181f77996370e5d4477143af8c8
yudylaw <20liuyu@163.com>
2015-12-28 16:19:38 +0800

    add clrs

---
+++ src/main/java/com/yudylaw/clrs/InsertSort.java
@@ -0,0 +1,35 @@
+
+package com.yudylaw.clrs;
+
+/**
+ * @author liuyu3@xiaomi.com
+ * @since 2015年12月28日
+ */
+
+public class InsertSort {
+
+    public static void main(String[] args) {
+        int[] arr = { 0, 23, 42, 10, 232, 2002, 3, 2 };
+
+        arr = sort(arr);
+        
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+
+    public static int[] sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int j = i;
+            //swap with sorted numbers
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+        return arr;
+    }
+
+}
